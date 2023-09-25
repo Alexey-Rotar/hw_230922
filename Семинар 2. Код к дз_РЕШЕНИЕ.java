c56e7c2f02dada69d9c3c178ef537af62774910a
alexey.rotar <ar_mail@inbox.ru>
2023-09-25 20:05:26 +0500

    v1.3

--- Семинар 2. Код к дз_РЕШЕНИЕ.java
+++ Семинар 2. Код к дз_РЕШЕНИЕ.java
@@ -3,7 +3,7 @@
 
 try {
     int d = 0;
-    double catchedRes1=intArray[8] / d;
+    double catchedRes1 = intArray[8] / d;
     System.out.println("catchedRes1 = " + catchedRes1);
 } catch(NullPointerException | ArrayIndexOutOfBoundsException | ArithmeticException e) {
     System.out.println("Catching exception: " + e);
